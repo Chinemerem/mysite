7f60413648ff8eedcc0d5fde6277cd4c3d1d3b99
Jessica <jessica@sefhare.bhp.org.bw>
2017-06-13 15:47:32 +0200

    testing

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    def was_published_rece
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - \
+                                datetime.timedelta(days= 1)
 
 @python_2_unicode_compatible
 class Choice(models.Model):
