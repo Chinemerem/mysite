54939f23cd7236e5b489e1feecf2f5d6bf68fa47
Jessica <[mildrednwonye@gmail.com]>
2017-06-15 16:48:56 +0200

    Updated some files and created templates

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 
+
+
 @python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,9 +13,16 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+
+    """def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)"""
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - \
-                                datetime.timedelta(days= 1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
 
 @python_2_unicode_compatible
 class Choice(models.Model):
